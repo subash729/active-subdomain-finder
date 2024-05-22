5f265f8647f794e22dd48e8679ffa103b3115c3c
subash2devopsintern <subaschy729@gmail.com>
2024-05-22 16:23:24 +0545

    amass install test

--- scripts/dependency-install.sh
+++ scripts/dependency-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+scan_store_dir=$HOME/information-gathering
+
 # ------- LARGE Banner display section start
 
 function print_separator {
@@ -18,26 +20,26 @@ function print_header {
 # Detection in Yellow color
 function print_init {
     local message="$1"
-    printf "\e[33m%s\e[0m\n" "$message"  
+    printf "\e[33m%s\e[0m\n" "$message"
 }
 
 # Intermediate in Blue color
 function print_intermediate {
     local message="$1"
-    printf "\e[34m%s\e[0m\n" "$message"  
+    printf "\e[34m%s\e[0m\n" "$message"
 }
 
 # Completion in Green color
 function print_success {
     local message="$1"
-    printf "\e[1m\e[32m%s\e[0m\n" "$message"  
+    printf "\e[1m\e[32m%s\e[0m\n" "$message"
     print_separator
 }
 
 # Failures in Red color
 function print_fail {
     local message="$1"
-    printf "\e[1m\e[31m%s\e[0m\n" "$message"  
+    printf "\e[1m\e[31m%s\e[0m\n" "$message"
     print_separator
 }
 
@@ -51,7 +53,7 @@ check_figlet_installed() {
     print_separator
     sleep 2
     if command -v figlet &>/dev/null; then
-        print_success "Figlet is already installed"  
+        print_success "Figlet is already installed"
         return 0
     else
         print_fail "Figlet Package is missing"
@@ -65,10 +67,24 @@ check_subfinder_installed() {
     print_separator
     sleep 2
     if command -v subfinder &>/dev/null; then
-        print_success "subfinder is already installed"  
+        print_success "subfinder is already installed"
         return 0
     else
         print_fail "subfinder Package is missing"
+        return 1
+    fi
+}
+
+check_amass_installed() {
+    print_separator
+    print_header "3 - AMASS"
+    print_separator
+    sleep 2
+    if command -v amass &>/dev/null; then
+        print_success "amass is already installed"
+        return 0
+    else
+        print_fail "amass Package is missing"
         return 1
     fi
 }
@@ -92,18 +108,18 @@ install_figlet() {
         print_separator
 
         if [ $? -eq 0 ]; then
-            print_success "Figlet is now installed"  
+            print_success "Figlet is now installed"
         else
-            print_fail "Failed to install Figlet"  
+            print_fail "Failed to install Figlet"
         fi
 
     elif grep -qEi 'redhat\|centos' /etc/os-release; then
         sudo yum -y install figlet
         print_separator
         if [ $? -eq 0 ]; then
-            print_success "Figlet is now installed"  
+            print_success "Figlet is now installed"
         else
-            print_fail "Failed to install Figlet"  
+            print_fail "Failed to install Figlet"
         fi
 
     elif grep -q 'Amazon Linux 2' /etc/os-release || grep -q 'Amazon Linux 3' /etc/os-release; then
@@ -111,13 +127,13 @@ install_figlet() {
         sudo yum -y install figlet
         print_separator
         if [ $? -eq 0 ]; then
-            print_success "Figlet is now installed"  
+            print_success "Figlet is now installed"
         else
-            print_fail "Failed to install Figlet"  
+            print_fail "Failed to install Figlet"
         fi
     else
         print_separator
-        print_fail "Unsupported Linux distribution"  
+        print_fail "Unsupported Linux distribution"
         exit 1
     fi
 }
@@ -130,7 +146,7 @@ install_subfinder(){
     os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
     print_init "$os_description Detected on your system"
     printf "\n"
-    print_intermediate "Installing Figlet"
+    print_intermediate "Installing subfinder"
     print_separator
 
     if grep -q 'Ubuntu\|Kali' /etc/os-release; then
@@ -139,9 +155,9 @@ install_subfinder(){
         print_separator
 
         if [ $? -eq 0 ]; then
-            print_success "subfinder is now installed"  
+            print_success "subfinder is now installed"
         else
-            print_fail "subfinder to install Figlet"  
+            print_fail "subfinder to install Figlet"
         fi
 
     elif grep -qEi 'redhat\|centos' /etc/os-release; then
@@ -152,23 +168,54 @@ install_subfinder(){
         sudo dnf -y install subfinder
         print_separator
         if [ $? -eq 0 ]; then
-            print_success "subfinder is now installed"  
+            print_success "subfinder is now installed"
         else
-            print_fail "subfinder to install Figlet"  
+            print_fail "subfinder to install Figlet"
         fi
-    
+
     else
         print_separator
-        print_fail "Unsupported Linux distribution"  
+        print_fail "Unsupported Linux distribution"
         exit 1
     fi
 
 }
 
+install_subfinder(){
+    if check_amass_installed; then
+        return
+    fi
+
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
+    printf "\n"
+    print_intermediate "Installing amass"
+    print_separator
+
+    if grep -q 'Ubuntu\|Kali' /etc/os-release; then
+        sudo apt-get update
+        sudo apt-get install -y amass
+        print_separator
+
+        if [ $? -eq 0 ]; then
+            print_success "amass is now installed"
+        else
+            print_fail "amass to install Figlet"
+        fi
+
+    else
+        print_separator
+        print_fail "Unsupported Linux distribution"
+        exit 1
+    fi
+
+}
+
+
 main() {
     #first package installation
     install_figlet
     install_subfinder
-    
+    install_amass
 }
 main
---
+++ scripts/scan-filter-subdomain.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+scan_store_dir=$HOME/information-gathering
+declare -a all_subdomain_array_files # for initial all subdomains
+declare -a unique_subdomain_array_files # removing duplicates and storing unique
+declare -a active_subdomain_array_files # filtering only active domains
+
+# ------- LARGE Banner display section start
+
+function print_separator {
+    printf "\n%s\n" "--------------------------------------------------------------------------------"
+}
+
+function print_header {
+    figlet -c -f slant "$1"
+    print_separator
+}
+
+# --------------- Large Banner display Section end --------------
+
+# Displaying Screen message in color Start
+
+# Detection in Yellow color
+function print_init {
+    local message="$1"
+    printf "\e[33m%s\e[0m\n" "$message"
+}
+
+# Intermediate in Blue color
+function print_intermediate {
+    local message="$1"
+    printf "\e[34m%s\e[0m\n" "$message"
+}
+
+# Completion in Green color
+function print_success {
+    local message="$1"
+    printf "\e[1m\e[32m%s\e[0m\n" "$message"
+    print_separator
+}
+
+# Failures in Red color
+function print_fail {
+    local message="$1"
+    printf "\e[1m\e[31m%s\e[0m\n" "$message"
+    print_separator
+}
+
+# -------------Displaying Screen message in color end ----------------
+
+prerequisite_setup() {
+    print_init "Creating Scan output Base directory and files at $scan_store_dir"
+    print_separator
+    mkdir -p $scan_store_dir
+
+    if [[ -f ../domain-list.txt ]]; then
+        domains=$(cat ../domain-list.txt)
+    else
+        echo "domain-list.txt not found!"
+        exit 1
+    fi
+
+    for domain in $domains; do
+        subdomain_single_file=$scan_store_dir/${domain}-initial_subdomain$(date +%Y-%m-%d__%H:%M).txt
+        unique_subdomain_file=$scan_store_dir/${domain}-unique_subdomain$(date +%Y-%m-%d__%H:%M).txt
+        active_subdomain_file=$scan_store_dir/${domain}-active_subdomain$(date +%Y-%m-%d__%H:%M).txt
+        touch $subdomain_single_file
+        touch $unique_subdomain_file
+        touch $active_subdomain_file
+
+        # Array used to store filenames
+        all_subdomain_array_files+=("$subdomain_single_file")
+        unique_subdomain_array_files+=("$unique_subdomain_file")
+        active_subdomain_array_files+=("$active_subdomain_file")
+    done
+}
+
+scanning_subdomain() {
+    print_separator
+    echo "Domains to be scanned:"
+    print_init "$domains"
+    print_separator
+
+    index=0
+    for domain in $domains; do
+        print_separator
+        print_intermediate "Scanning $domain in progress..."
+        subdomain_single_file=${all_subdomain_array_files[$index]}
+        subfinder -d $domain >> "$subdomain_single_file"
+        print_separator
+        echo -e "\n\n"
+        index=$((index + 1))
+    done
+}
+
+filtering_duplicate_and_inactive(){
+    print_separator
+    print_intermediate "STEP -1 : Filtering duplicate sub-domains in progress..."
+    index=0
+    for domain in $domains; do
+        print_intermediate "Processing $domain sub-domains in progress..."
+        subdomain_single_file=${all_subdomain_array_files[$index]}
+        unique_subdomain_file=${unique_subdomain_array_files[$index]}
+        
+        # Redirecting unique output to respective subdomain-file
+        sort "$subdomain_single_file" | uniq >> "$unique_subdomain_file"
+        index=$((index + 1))
+    done
+    print_separator
+    echo -e "\n\n"
+
+    print_separator
+    print_intermediate "STEP -2 : Finding active subdomains in progress..."
+    index=0
+    for domain in $domains; do
+        print_intermediate "Processing $domain sub-domains in progress..."
+        unique_subdomain_file=${unique_subdomain_array_files[$index]}
+        active_subdomain_file=${active_subdomain_array_files[$index]}
+        # Add logic to filter active subdomains and write to $active_subdomain_file
+        index=$((index + 1))
+    done
+}
+
+deleting_others_scan() {
+    echo "hello delete"
+}
+
+main() {
+    prerequisite_setup
+    scanning_subdomain
+    filtering_duplicate_and_inactive
+    deleting_others_scan
+}
+
+main
