ad6d10b7133e3797068c3cc76085c3366929a30c
subash2devopsintern <subaschy729@gmail.com>
2024-05-22 13:45:06 +0545

    subfinder install: sucess

--- scripts/upgrade-install.sh
+++ scripts/upgrade-install.sh
@@ -87,11 +87,49 @@ install_and_upgrade_figlet() {
         exit 1
     fi
 }
+install_subfinder(){
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
+    printf "\n"
+    print_intermediate "Installing Figlet"
+    print_separator
+
+    if grep -q 'Ubuntu\|Kali' /etc/os-release; then
+        sudo apt-get update
+        sudo apt-get install -y subfinder
+        print_separator
+
+        if [ $? -eq 0 ]; then
+            print_success "subfinder is now installed"  
+        else
+            print_fail "subfinder to install Figlet"  
+        fi
+
+    elif grep -qEi 'redhat\|centos' /etc/os-release; then
+        sudo yum -y install subfinder
+        sudo dnf -y update
+        sudo dnf -y install epel-release
+        sudo dnf -y update
+        sudo dnf -y install subfinder
+        print_separator
+        if [ $? -eq 0 ]; then
+            print_success "subfinder is now installed"  
+        else
+            print_fail "subfinder to install Figlet"  
+        fi
+    
+    else
+        print_separator
+        print_fail "Unsupported Linux distribution"  
+        exit 1
+    fi
+
+}
 
 main() {
     #first package installation
     install_and_upgrade_figlet
-    # second package install
+    install_subfinder
     
 }
 main
