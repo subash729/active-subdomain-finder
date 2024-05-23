fcb7eb23f28f31904b0af07e01c48fc327d9f455
subash2devopsintern <subaschy729@gmail.com>
2024-05-23 11:39:39 +0545

    ubuntu runner test

--- scripts/0.runner.sh
+++ scripts/0.runner.sh
@@ -8,17 +8,17 @@ echo -e "Step 1: Folder created.\n----------------------------------------------
 # Download the latest runner package
 echo -e "Step 2: Downloading the latest runner package...\n"
 sudo apt install -y curl
-curl -o actions-runner-linux-x64-2.316.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.0/actions-runner-linux-x64-2.316.0.tar.gz
+curl -o actions-runner-linux-x64-2.316.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz
 echo -e "Step 2: Download completed.\n-----------------------------------------------------------------"
 
 # Optional: Validate the hash
 echo -e "Step 3: Validating hash...\n"
-echo "64a47e18119f0c5d70e21b6050472c2af3f582633c9678d40cb5bcb852bcc18f  actions-runner-linux-x64-2.316.0.tar.gz" | shasum -a 256 -c
+echo "d62de2400eeeacd195db91e2ff011bfb646cd5d85545e81d8f78c436183e09a8  actions-runner-linux-x64-2.316.1.tar.gz" | shasum -a 256 -c
 echo -e "Step 3: Hash validated.\n-----------------------------------------------------------------"
 
 # Extract the installer
 echo -e "Step 4: Extracting the installer...\n"
-tar xzf ./actions-runner-linux-x64-2.316.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.316.1.tar.gz
 echo -e "Step 4: Installer extracted.\n-----------------------------------------------------------------"
 
 echo -e "=================TASK completed Sucessfully======================"
